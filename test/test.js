b5406db17ae86a68c6afdb198af8df3905e1bd68
Ryan VanGundy <ryanvangundy@Ryans-MacBook-Pro.local>
2013-12-06 16:31:28 -0500

    Testing fade directive

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var assert = chai.assert;
 var dir    = require('../index.js');
 var page   = require('page');
 
+page.base('/test');
+
 /*******************************
  *  Basic Object Construction  *
  *******************************/
@@ -17,10 +19,10 @@ describe('Directives', function() {
         div2.innerHTML = 'test';
 
         it('should load an element in to a parent element when a page has triggered', function() {
-            page('/', dir.load(div, div2), function() {
+            page('/load', dir.load(div, div2), function() {
                 assert.equal(div.children[0].innerHTML, 'test');
             });
-            page('/');
+            page('/load');
         });
     });
 
@@ -31,23 +33,24 @@ describe('Directives', function() {
         div.appendChild(div2);
 
         it('should clear all child elements from the parent element', function() {
-            page('/', dir.clear(div), function() {
+            page('/clear', dir.clear(div), function() {
                 assert.equal(div.children.length, 0);
             });
-            page('/');
+            page('/clear');
         });
     });
 
     describe('fade()', function() {
         var div = document.createElement('div');
 
-        div.innerHTML = 'test';
+        it('should transition opacity of element to .5 in 500ms', function(ok) {
+            page('/fade', dir.fade(div,[0,0.5], 500));
+            page('/fade');
 
-        it('should transition opacity of element to .5 in 500ms', function() {
-            page('/', dir.fade(div,[0,.5], 500), function() {
-                assert.equal(div.style.opacity, .5);
-            });
-            page('/');
+            setTimeout(function() {
+                assert.equal(div.style.opacity, 0.5);
+                ok();
+            }, 600);
         });
     });
 });
